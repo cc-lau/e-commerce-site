8ea640d69cc6494f2f95fb14ff17a51d34241640
cc-lau <christianlau03@gmail.com>
2023-03-01 11:06:37 -0700

    Updated App.jsx to include navbar and product display components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import NavBarView from "./Components/NavBar/NavBarView";
+import { ProductsDisplay } from "./Components/ProductsDisplay/ProductsDisplay";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <NavBarView />
+      <ProductsDisplay />
+    </div>
+  );
 }
 
 export default App;
