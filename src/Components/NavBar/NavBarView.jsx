f29039587a6552e8bc0681d08f14b1abb2f3a4bf
cc-lau <christianlau03@gmail.com>
2023-03-01 11:03:25 -0700

    Nav Bar Setup

---
+++ src/Components/NavBar/NavBarView.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./NavBarStyles.css";
+
+function NavBarView() {
+  return (
+    <div>
+      <div className="nav-bar">
+        <h1>Music Store</h1>
+        <h2>Products</h2>
+      </div>
+    </div>
+  );
+}
+
+export default NavBarView;
