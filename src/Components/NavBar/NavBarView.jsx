c38af2f75bb609c151717701c4824099bca5849e
cc-lau <christianlau03@gmail.com>
2023-03-02 21:30:10 -0700

    Added product options/search and cart to navbar

--- src/Components/NavBar/NavBarView.jsx
+++ src/Components/NavBar/NavBarView.jsx
@@ -1,12 +1,33 @@
 import React from "react";
 import "./NavBarStyles.css";
+import Icon from "@mdi/react";
+import { mdiCart } from "@mdi/js";
 
 function NavBarView() {
   return (
     <div>
       <div className="nav-bar">
         <h1>Music Store</h1>
-        <h2>Products</h2>
+        <ul className="product-options">
+          <li>
+            <a>Plugins</a>
+          </li>
+          <li>
+            <a>Synthesizers</a>
+          </li>
+          <li>
+            <a>Equipment</a>
+          </li>
+        </ul>
+        <div className="search-cart-container">
+          <input
+            type="search"
+            className="product-search"
+            id="product-search"
+            placeholder="Search Products..."
+          />
+          <Icon path={mdiCart} size={1} />
+        </div>
       </div>
     </div>
   );
