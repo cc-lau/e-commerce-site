1710b8081eaf045195e19087d7c191e9d6191584
cc-lau <christianlau03@gmail.com>
2023-03-03 11:28:49 -0700

    Implemented React router to route between display pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import "./App.css";
+import PluginsDisplay from "./Components/AudioPlugins/PluginsDisplay";
+import Cart from "./Components/Cart/Cart";
+import EquipmentDisplay from "./Components/Equipment/EquipmentDisplay";
+import Home from "./Components/Home/Home";
 import NavBarView from "./Components/NavBar/NavBarView";
-import { ProductsDisplay } from "./Components/ProductsDisplay/ProductsDisplay";
+import SynthesizersDisplay from "./Components/Synthesizers/SynthesizersDisplay";
 
 function App() {
   return (
     <div className="App">
-      <NavBarView />
-      <ProductsDisplay />
+      <BrowserRouter>
+        <NavBarView />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="plugins" element={<PluginsDisplay />} />
+          <Route path="synthesizers" element={<SynthesizersDisplay />} />
+          <Route path="equipment" element={<EquipmentDisplay />} />
+          <Route path="cart" element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
