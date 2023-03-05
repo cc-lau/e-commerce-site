1aba3f2245c0d164022dd1a35a2631eaec5e4837
cc-lau <christianlau03@gmail.com>
2023-03-05 16:00:06 -0700

    Added route to display a single product based on the products ID

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Cart from "./Components/Cart/Cart";
 import EquipmentDisplay from "./Components/Equipment/EquipmentDisplay";
 import Home from "./Components/Home/Home";
 import NavBarView from "./Components/NavBar/NavBarView";
+import ProductView from "./Components/ProductView/ProductView";
 import SynthesizersDisplay from "./Components/Synthesizers/SynthesizersDisplay";
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
           <Route path="plugins" element={<PluginsDisplay />} />
           <Route path="synthesizers" element={<SynthesizersDisplay />} />
           <Route path="equipment" element={<EquipmentDisplay />} />
+          <Route path="/product/:productId" element={<ProductView />} />
           <Route path="cart" element={<Cart />} />
         </Routes>
       </BrowserRouter>
