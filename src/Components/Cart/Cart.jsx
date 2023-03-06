d465dd6fe13d5cd5a6fb81bf59f899e801a12c45
cc-lau <christianlau03@gmail.com>
2023-03-06 11:46:30 -0700

    Created component to display users items in their cart

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 function Cart() {
-  return <div>Cart</div>;
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    console.log(cart);
+  }, []);
+
+  return (
+    <div>
+      <h2>Cart: </h2>
+      <div className="product-cards">
+        <ul>
+          {cart.cartItems.map((cartItems) => (
+            <li key={cartItems.product.id}>
+              <Link to={`/product/${cartItems.product.id}`}>
+                <img
+                  className="product-img"
+                  src={cartItems.product.image}
+                ></img>
+                {cartItems.product.title + " "}
+                {"$" + cartItems.product.price}
+                {" Quantity " + cartItems.qty}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <Link to={"/checkout"} className="check-out-button">
+          Checkout
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Cart;
