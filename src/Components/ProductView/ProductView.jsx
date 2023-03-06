0101ff99c5bbef5dd312f9ad269631931aa49a3b
cc-lau <christianlau03@gmail.com>
2023-03-06 11:48:06 -0700

    Created an input field for item quantity and a button to add item to cart along with quantity of the item.

--- src/Components/ProductView/ProductView.jsx
+++ src/Components/ProductView/ProductView.jsx
@@ -1,17 +1,28 @@
-import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchSingleProduct } from "../ProductsDisplay/ProductsSlice";
+import "./ProductViewStyles.css";
+import { addToCart } from "../Cart/CartSlice";
 
 function ProductView() {
   const product = useSelector((state) => state.product);
   const dispatch = useDispatch();
   let { productId } = useParams();
 
+  const cart = useSelector((state) => state.cart);
+
+  const [qty, setQty] = useState(1);
+
   useEffect(() => {
     dispatch(fetchSingleProduct(productId));
-    console.log(product);
+    /* console.log(product); */
   }, []);
+
+  useEffect(() => {
+    console.log(cart);
+  }, []);
+
   return (
     <div>
       <div className="products-container">
@@ -22,10 +33,23 @@ function ProductView() {
         {!product.loading && product.products ? (
           <div className="product-card">
             <img className="product-img" src={product.products.image}></img>
-            {product.products.title + " "}
-            {"$" + product.products.price}
-
-            <button>Add to cart</button>
+            <p> {product.products.title}</p>
+            <p> {"$" + product.products.price}</p>
+            <input
+              className="qty-input"
+              type="number"
+              value={qty}
+              onChange={(e) => setQty(parseInt(e.target.value))}
+            ></input>
+            <Link to="/cart">
+              <button
+                onClick={() =>
+                  dispatch(addToCart({ product: product.products, qty: qty }))
+                }
+              >
+                Add to cart
+              </button>
+            </Link>
           </div>
         ) : null}
       </div>
