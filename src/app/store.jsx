ba3947ab3150ab491772406fce6c1e89eedf63dd
cc-lau <christianlau03@gmail.com>
2023-03-06 11:45:36 -0700

    Added a cart reducer

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,9 +1,11 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, createReducer } from "@reduxjs/toolkit";
 import productReducer from "../Components/ProductsDisplay/ProductsSlice";
+import cartReducer from "../Components/Cart/CartSlice";
 
 const store = configureStore({
   reducer: {
     product: productReducer,
+    cart: cartReducer,
   },
 });
 
