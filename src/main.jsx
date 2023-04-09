da8f30991766e727b9e7d2fff858868b60ba444d
cc-lau <christianlau03@gmail.com>
2023-04-09 16:41:33 -0600

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,9 @@ import store from "./app/store";
 import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>
 );
