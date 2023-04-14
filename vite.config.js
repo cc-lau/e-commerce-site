e55c889c4cec652fdf18b09281f7e65e55af44da
cc-lau <christianlau03@gmail.com>
2023-04-13 21:08:18 -0600

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
 export default defineConfig({
+  build: {
+    commonjsOptions: {
+      defaultIsModuleExports(id) {
+        const module = require(id);
+        if (module?.default) {
+          return false;
+        }
+        return "auto";
+      },
+    },
+  },
   plugins: [react()],
-  base: "/e-commerce-site/",
 });
